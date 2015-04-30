014c8e59ad8dc5f275dc32849fde73a9a2a62f9f
net4u <laurentiu.lazar@infosystems4u.ro>
2015-04-30 23:36:31 +0300

    Correct typo

--- GMap/App_Start/BundleConfig.cs
+++ GMap/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace GMap
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/custom.css"));
         }
     }
 }
